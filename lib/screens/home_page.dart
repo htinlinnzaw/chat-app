09430bfdeca70d2655bcb30b72e2fc398b97219f
HtinLinnZaw <htinlinnzaw.dev@gmail.com>
2021-01-26 23:52:38 +0630

    1. add: navigation bar and items [progress] @ home_page.dart

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -8,5 +8,18 @@ class HomePage extends StatelessWidget {
             child: const Text('Chat'),
           ),
         ),
+    bottomNavigationBar: BottomNavigationBar(
+      selectedItemColor: Colors.red,
+      // ignore: prefer_const_literals_to_create_immutables
+      items: [
+        const BottomNavigationBarItem(
+            icon: Icon(Icons.message), title: Text('Chats'),
+        ),
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.group_work
+          ), title: Text('Channels'),
+        ),
+      ],
+    ),
       );
 }
